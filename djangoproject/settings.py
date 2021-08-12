eb20dcab7d298fd9412c0fc24812e2c5d6ef905d
Abdulhafiz-Yuuf <talk2abdulhafiz@gmail.com>
2021-08-12 15:38:18 +0100

    added environment variable

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -11,11 +11,15 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 
+import environ
 import django_heroku
 import dj_database_url
 import os
 from pathlib import Path
 
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-is-lc2!fxtex4z!$1!on-4@bi_7_o)j#2c#bs8$#epspzti^e)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'djangodep.herokuapp.com']
 
@@ -82,22 +86,14 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': 'learningDjango',
-
-        'USER': 'postgres',
-
-        'PASSWORD': 'newPassword',
-
-        'HOST': 'localhost',
-
-        'PORT': '5432',
-
-    }
+DATABASES = {'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': env('DATABASE_NAME'),
+    'USER': env('DATABASE_USER'),
+    'PASSWORD': env('DATABASE_PASS'),
+    'HOST': 'localhost',
+    'PORT': '5432',
+}
 }
 
 
