f48af25651755e41af1000cf822ffb5c291fe43e
Abdulhafiz-Yuuf <talk2abdulhafiz@gmail.com>
2021-08-12 15:04:46 +0100

    deactivating django_heroku after making sure static and staticfiles directory are availabe on github

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -149,4 +149,4 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 # Activate Django-Heroku.
-django_heroku.settings(locals(), staticfiles=False)  # in end of settings.py
+# django_heroku.settings(locals(), staticfiles=False)  # in end of settings.py
