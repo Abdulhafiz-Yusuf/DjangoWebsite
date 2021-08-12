98ce6d7b2ba8678bd76303cde13f69af99e0abda
Abdulhafiz-Yuuf <talk2abdulhafiz@gmail.com>
2021-08-12 17:25:47 +0100

    Refactoring settings.py

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 
+from django.core.management.utils import get_random_secret_key
 import environ
 import django_heroku
 import dj_database_url
@@ -27,12 +28,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
+# print(get_random_secret_key())
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-is-lc2!fxtex4z!$1!on-4@bi_7_o)j#2c#bs8$#epspzti^e)'
-
+SECRET_KEY = env('SECRET_KEY')
+value = env('DEBUG_VALUE')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if env('DEBUG_VALUE') == 'True' else False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'djangodep.herokuapp.com']
 
