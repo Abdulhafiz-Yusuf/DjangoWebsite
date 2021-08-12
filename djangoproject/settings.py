776793aedbd5b950e16763d76a0eca70663f2590
Abdulhafiz-Yuuf <talk2abdulhafiz@gmail.com>
2021-08-12 12:34:43 +0100

    Debug = True, Deactiviate djangoHeroku

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-is-lc2!fxtex4z!$1!on-4@bi_7_o)j#2c#bs8$#epspzti^e)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'djangodep.herokuapp.com']
 
